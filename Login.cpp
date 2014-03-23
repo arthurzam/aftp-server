a41afd919c4514ae63a781dfd1737b5bd648da18
Arthur Zamarin <arthurzam@gmail.com>
2014-03-23 10:57:41 +0200

    Add advanced login
    the base for advanced Login, with permissions, Login type, restricted
    folders

---
+++ Login.cpp
@@ -0,0 +1,109 @@
+#include "Login.h"
+
+Login::Login(const char* username, const byte_t* password, LOGIN_ACCESS state)
+{
+	strcpy(this->username, username);
+	memcpy(this->password, password, MD5_RESULT_LENGTH);
+	this->state = state;
+	this->restrictedFolders = NULL;
+	this->restrictedFoldersCount = 0;
+	this->isInit = TRUE;
+}
+
+Login::Login(FILE* srcFile)
+{
+	if(srcFile)
+	{
+		char folder[FILENAME_MAX + 1];
+		byte_t i;
+		fread(this->username, 1, USERNAME_MAX_LENGTH, srcFile);
+		fread(this->password, 1, MD5_RESULT_LENGTH, srcFile);
+		fread(&this->state, 1, sizeof(this->state), srcFile);
+		fread(&i, 1, sizeof(i), srcFile);
+
+		this->restrictedFolders = NULL;
+		this->restrictedFoldersCount = 0;
+		for(; i; i--)
+		{
+			fgets(folder, FILENAME_MAX + 1, srcFile);
+			this->addRestrictedFolder(folder);
+		}
+		this->isInit = TRUE;
+	}
+	else
+	{
+		this->password[0] = 0;
+		this->restrictedFolders = NULL;
+		this->state = LOGIN_ACCESS_READ_ONLY;
+		this->username[0] = 0;
+		this->restrictedFoldersCount = 0;
+		this->isInit = FALSE;
+	}
+}
+
+Login::~Login()
+{
+	folder* temp;
+	while(this->restrictedFolders)
+	{
+		temp = this->restrictedFolders;
+		this->restrictedFolders = this->restrictedFolders->next;
+		delete temp;
+	}
+}
+
+bool_t Login::check(const char* username, const byte_t* password) const
+{
+	return (!(memcmp(this->password, password, MD5_RESULT_LENGTH) || strcmp(this->username, username)));
+}
+
+void Login::addRestrictedFolder(const char* dir)
+{
+	folder* newOne = (folder*)malloc(sizeof(folder));
+	folder* temp = this->restrictedFolders;
+	memcpy(newOne->folder, dir, FILENAME_MAX);
+	newOne->folder[FILENAME_MAX] = 0;
+	newOne->folderLen = strlen(dir);
+	newOne->next = NULL;
+	if(!temp)
+	{
+		this->restrictedFolders = newOne;
+	}
+	else
+	{
+		while(temp->next)
+			temp = temp->next;
+		temp->next = newOne;
+	}
+	this->restrictedFoldersCount++;
+}
+
+bool_t Login::isRestrictedFolder(const char* path) const
+{
+	if(this->state == LOGIN_ACCESS_ADMIN)
+		return (FALSE);
+	folder* temp;
+	for(temp = this->restrictedFolders; temp; temp = temp->next)
+	{
+		if(!strncmp(path, temp->folder, temp->folderLen))
+			return (TRUE);
+	}
+	return (FALSE);
+}
+
+bool_t Login::save(FILE* dstFile) const
+{
+	if(!dstFile)
+		return (FALSE);
+	fwrite(this->username, 1, USERNAME_MAX_LENGTH, dstFile);
+	fwrite(this->password, 1, MD5_RESULT_LENGTH, dstFile);
+	fwrite(&this->state, 1, sizeof(this->state), dstFile);
+	fwrite(&this->restrictedFoldersCount, 1, sizeof(this->restrictedFoldersCount), dstFile);
+	folder* temp;
+	for(temp = this->restrictedFolders; temp; temp = temp->next)
+	{
+		fputs(temp->folder, dstFile);
+		fputc('\n', dstFile);
+	}
+	return (TRUE);
+}
---
+++ Login.h
@@ -0,0 +1,52 @@
+#ifndef LOGIN_H_
+#define LOGIN_H_
+
+#include <cstdio>
+#include <cstring>
+#include "defenitions.h"
+#include "md5.h"
+
+#define USERNAME_MAX_LENGTH 32
+
+enum LOGIN_ACCESS{
+	LOGIN_ACCESS_ADMIN = 0,
+	LOGIN_ACCESS_READ_ONLY,
+	LOGIN_ACCESS_LIMITED,
+	LOGIN_ACCESS_LIMITED_READ_ONLY,
+	LOGIN_ACCESS_ALL
+};
+
+/*
+ * A class for holding login data, the permissions per Login and etc'.
+ */
+class Login {
+	typedef struct _folder{
+		char folder[FILENAME_MAX + 1];
+		unsigned short folderLen;
+		struct _folder* next;
+	} folder;
+private:
+	char username[USERNAME_MAX_LENGTH];
+	byte_t password[MD5_RESULT_LENGTH];
+	byte_t state;
+	folder* restrictedFolders;
+	byte_t restrictedFoldersCount;
+	bool_t isInit;
+public:
+	Login(const char* username, const byte_t* password, LOGIN_ACCESS state);
+	Login(const Login& other);
+	Login(FILE* srcFile);
+	~Login();
+
+	bool_t check(const char* username, const byte_t* password) const;
+
+	/*
+	 * the folder path should be the relative one!
+	 */
+	void addRestrictedFolder(const char* folder);
+	bool_t isRestrictedFolder(const char* path) const;
+
+	bool_t save(FILE* dstFile) const;
+};
+
+#endif /* LOGIN_H_ */
