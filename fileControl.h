3698243c51ca601a648bfec7bb7a027d65049952
Arthur Zamarin <arthurzam@gmail.com>
2015-01-23 15:45:50 +0200

    fileControl as extern C
    move fileControl.h functions as extern C.

--- fileControl.h
+++ fileControl.h
@@ -5,31 +5,31 @@
 #include "IOThreadPool.h"
 
 class User;
-
-/*
- * returns the real folder of the given path, in the result array
- */
-bool getRealDirectory(char* realativDirectory, char* result);
-bool isDirectory(const char* directory);
-bool isFileExists(const char* path);
+extern "C" {
+    /*
+     * returns the real folder of the given path, in the result array
+     */
+    bool getRealDirectory(char* realativDirectory, char* result);
+    bool isDirectory(const char* directory);
+    bool isFileExists(const char* path);
 
 #ifndef WIN32
-/*
- * only for Unix based OS
- */
-void symbolicLink(fsData* data);
+    /*
+     * only for Unix based OS
+     */
+    void symbolicLink(fsData* data);
 #endif
 
-void getContentDirectory(fsData* data);
-void createDirectory(fsData* data);
+    void getContentDirectory(fsData* data);
+    void createDirectory(fsData* data);
 #define moveDirectory moveFile
-void removeFolder(fsData* data);
-void copyFolder(fsData* data);
+    void removeFolder(fsData* data);
+    void copyFolder(fsData* data);
 
-void moveFile(fsData* data);
-void copyFile(fsData* data);
-void removeFile(fsData* data);
-void getFilesize(fsData* data);
-void getMD5OfFile(fsData* data);
-
+    void moveFile(fsData* data);
+    void copyFile(fsData* data);
+    void removeFile(fsData* data);
+    void getFilesize(fsData* data);
+    void getMD5OfFile(fsData* data);
+}
 #endif
